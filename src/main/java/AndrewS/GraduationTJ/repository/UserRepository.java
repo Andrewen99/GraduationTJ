7f1e66775fcdc15ab2642153849aaf3de5279068
Andrew Shakirov <ewen9936@gmail.com>
2019-08-06 16:19:11 +1100

    08 Data-jpa + UserService

--- src/main/java/AndrewS/GraduationTJ/repository/UserRepository.java
+++ src/main/java/AndrewS/GraduationTJ/repository/UserRepository.java
@@ -2,8 +2,28 @@ package AndrewS.GraduationTJ.repository;
 
 //Waiting for data-jpa
 
+import AndrewS.GraduationTJ.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class UserRepository {
+    private static final Sort SORT_NAME_EMAIL = new Sort(Sort.Direction.ASC, "name", "email");
+
+    @Autowired
+    private CrudUserRepository repository;
+
+    public User save(User user) {return repository.save(user);}
+
+    public boolean delete(int id) {return repository.delete(id) != 0;}
+
+    public User get(int id) {return repository.findById(id).orElse(null);}
+
+    public User getByEmail(String email) {return repository.getByEmail(email);}
+
+    public List<User> getAll() {return repository.findAll(SORT_NAME_EMAIL);}
+
 }
--- src/main/java/AndrewS/GraduationTJ/service/UserService.java
+++ src/main/java/AndrewS/GraduationTJ/service/UserService.java
@@ -5,38 +5,45 @@ import AndrewS.GraduationTJ.repository.UserRepository;
 import AndrewS.GraduationTJ.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
+
+import static AndrewS.GraduationTJ.util.ValidationUtil .checkNotFound;
+import static AndrewS.GraduationTJ.util.ValidationUtil .checkNotFoundWithId;
 
 //     Class is not finished and will work after data-jpa
 
 @Service
 public class UserService {
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
-    public User create() {
-        return null;
+    public User create(User user) {
+        Assert.notNull(user, "user must not be null");
+        return repository.save(user);
     }
 
-    public void delete() throws NotFoundException {
-
+    public void delete(int id) throws NotFoundException {
+        checkNotFoundWithId(repository.get(id), id);
     }
 
     public User get(int id) throws NotFoundException {
-        return null;
+        return checkNotFoundWithId(repository.get(id),id);
     }
 
     public User getByEmail(String email) throws NotFoundException {
-        return null;
+        Assert.notNull(email, "email must not be null");
+        return checkNotFound(repository.getByEmail(email), "email = " + email);
     }
 
     public List<User> getAll() {
-        return null;
+        return repository.getAll();
     }
 
     public void update(User user) throws NotFoundException {
-
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(repository.save(user), user.getId());
     }
 
 }
